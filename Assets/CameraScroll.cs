75ac730615b1d1457a61ca00c6fefbd1814836c2
Nguyễn Minh Tuấn <ngminhtuan1128@gmail.com>
2025-03-08 22:05:27 +0700

    Update(Scene): Level2

--- Assets/CameraScroll.cs
+++ Assets/CameraScroll.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraScroll : MonoBehaviour
 {
@@ -10,23 +8,21 @@ public class CameraScroll : MonoBehaviour
 
     private Camera mainCamera;
     private float killZoneX;
+    private bool isScrolling = true; // Biến kiểm soát việc cuộn camera
 
     void Start()
     {
-        // Lấy component Camera
         mainCamera = GetComponent<Camera>();
-
-        // Tính toán vị trí ban đầu của vùng giết nhân vật
-        CalculateKillZone();
     }
 
     void Update()
     {
-        // Di chuyển camera theo tốc độ cuộn
-        transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
-
-        // Cập nhật vị trí vùng giết nhân vật
-        CalculateKillZone();
+        // Kiểm tra nếu đang cuộn camera
+        if (isScrolling)
+        {
+            // Di chuyển camera theo tốc độ cuộn
+            transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
+        }
 
         // Kiểm tra xem người chơi có vượt ra khỏi vùng giết nhân vật không
         if (player != null)
@@ -35,41 +31,18 @@ public class CameraScroll : MonoBehaviour
         }
     }
 
-    void CalculateKillZone()
-    {
-        // Tính toán vị trí X của vùng giết nhân vật (cạnh trái của màn hình + offset)
-        float cameraLeftEdge = transform.position.x - mainCamera.orthographicSize * mainCamera.aspect;
-        killZoneX = cameraLeftEdge + killZoneOffset;
-    }
-
     void CheckPlayerPosition()
     {
         // Nếu nhân vật đi ra khỏi vùng giết nhân vật (bên trái màn hình)
         if (player.position.x < killZoneX)
         {
-            // Tạm thời chỉ in thông báo trong console
             Debug.Log("Player would be killed here!");
-
-            // Khi bạn đã tạo phương thức Die(), hãy gọi:
-            // PlayerScript playerScript = player.GetComponent<PlayerScript>();
-            // if (playerScript != null)
-            // {
-            //     playerScript.Die();
-            // }
         }
     }
 
-    // Hiển thị vùng giết nhân vật trong Editor
-    void OnDrawGizmos()
+    // Phương thức dừng cuộn camera
+    public void StopScrolling()
     {
-        if (Application.isPlaying && mainCamera != null)
-        {
-            Gizmos.color = Color.red;
-            float cameraHeight = 2f * mainCamera.orthographicSize;
-            Gizmos.DrawLine(
-                new Vector3(killZoneX, transform.position.y - cameraHeight / 2, 0),
-                new Vector3(killZoneX, transform.position.y + cameraHeight / 2, 0)
-            );
-        }
+        isScrolling = false;
     }
 }
